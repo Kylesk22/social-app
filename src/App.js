14b50a6199ec4df6151ebbf239ca685467551fd8
Kylesk22 <105800286+Kylesk22@users.noreply.github.com>
2022-09-02 20:13:13 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import SpotterNav from './Components/Navbar';
+import ProfilePic from './Components/ProfilePicture';
+function App() {
+  return (
+    <div className="App" id="App">
+        <SpotterNav />
+        <ProfilePic />
+    </div>
+  );
+}
+
+export default App;
