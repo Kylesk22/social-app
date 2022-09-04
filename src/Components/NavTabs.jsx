811d238718e15c3686adb24d72cfe89384cf0074
SantaGosu <taz.c.munoz@gmail.com>
2022-09-04 10:33:01 +0000

    will mess with background image later

--- src/Components/NavTabs.jsx
+++ src/Components/NavTabs.jsx
@@ -4,13 +4,13 @@ import Tabs from 'react-bootstrap/Tabs';
 
 const NavTabs = () => {
     return(
-    <div classNameNameName="container">
+    <div className="container-fluid">
         <Tabs
       defaultActiveKey="profile"
       id="uncontrolled-tab-example"
       className="mb-3"
     >
-      <Tab eventKey="home" title="Home">
+      <Tab eventKey="home" title="Home" id="tabHome" >
         this is the home page
       </Tab>
       <Tab eventKey="profile" title="Profile">
